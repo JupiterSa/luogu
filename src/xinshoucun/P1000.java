0bf3a1ec43d0bc42fecc28c74eba6576c60cb103
mo.liu <1121759144@qq.com>
2019-03-09 14:02:08 +0800

    FIRST COMMIT

---
+++ src/xinshoucun/P1000.java
@@ -0,0 +1,13 @@
+package xinshoucun;
+
+public class P1000 {
+/*
+ * 超级玛丽是一个非常经典的游戏。请你用字符画的形式输出超级玛丽中的一个场景
+ * 
+ * */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
